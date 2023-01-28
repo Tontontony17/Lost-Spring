62607c97414c190d8b61edafe4a392c0f9b68a2d
Tontontony17 <anthonyrusso2009@gmail.com>
2023-01-28 18:19:10 +0100

    Proto V4

--- Scene/CharacterBody3D.gd
+++ Scene/CharacterBody3D.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 7
-const JUMP_VELOCITY = 8.5
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -19,7 +19,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
